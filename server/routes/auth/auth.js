798dfde0796112a5895510331c02251402d245e7
Amitpotdukhe <69644033+Amitpotdukhe@users.noreply.github.com>
2024-01-05 23:33:14 +0530

    forgot password

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -83,24 +83,51 @@ authRouter.post('/api/verify-otp', async (req, res) => {
 // SIGN UP
 authRouter.post("/api/signup", async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, mobile, password, otp } = req.body;
+    console.log(req.body, typeof otp);
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
+    const existingUser = await User.findOne({
+      $or: [
+        { email: email },
+        { mobile: mobile }
+      ]
+    });
+    if (existingUser || existingUser.length > 0) {
       return res
         .status(400)
-        .json({ msg: "User with same email already exists!" });
+        .json({ msg: "User with same email or mobile already exists!" });
     }
+
+    const response = await otpVerification.find({ email }).sort({ createdAt: -1 }).limit(1);
+    console.log("otp is :", response);
+    if (response.length === 0) {
+      return res.status(400).json({
+        success: false,
+        msg: "The OTP is not valid",
+      });
+    } else if (otp != response[0].otp) {
+      return res.status(400).json({
+        success: false,
+        msg: "The OTP is not valid",
+      });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
 
     let user = new User({
       email,
-      password,
+      mobile,
+      password: hashedPassword,
       name,
     });
     user = await user.save();
-    await sendEmail(email, "", "", "welcome to our platform test email asdnsadijasdn.");
-    res.json(user);
+    await sendEmail(email, "", "", "welcome to homeLyf services.")
+
+    const token = jwt.sign({ id: user.email }, "passwordKey");
+
+    res.status(201).json({ user, token });
   } catch (e) {
+    console.log(e);
     res.status(500).json({ error: e.message });
   }
 });
@@ -129,6 +156,43 @@ authRouter.post("/api/signin", async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 });
+
+authRouter.post("/api/forgotpassword", async (req, res) => {
+  try {
+    const { email, newPassword, otp } = req.body;
+    if (!email || !newPassword || !otp) {
+      return res.status(400).json({ msg: "Enter valid email or password." })
+    }
+
+    const existingUser = await User.findOne({
+      $or: [
+        { email: email }
+      ]
+    });
+    console.log(existingUser);
+    if (existingUser == null || existingUser.length == 0) {
+      return res.status(400).json({ msg: "User with email does not exist.." });
+    }
+
+    const response = await otpVerification.find({ email }).sort({ createdAt: -1 }).limit(1);
+    console.log("otp is :", response);
+    if (!response || response.length == 0 || response[0].otp !== otp) {
+      return res.status(400).json({ msg: "Failed to verify otp." })
+    }
+
+    const hashedPassword = await bcryptjs.hash(newPassword, 8);
+    let doc = await User.findOneAndUpdate({ email: email }, { password: hashedPassword });
+    console.log("updated : ", doc);
+    if (!doc || doc.length == 0) {
+      return res.status(400).json({ msg: "Failed to update password." });
+    }
+
+    return res.status(200).json({ msg: "Password updated successfully." })
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ msg: "Failed to update password" })
+  }
+})
 
 authRouter.post("/tokenIsValid", async (req, res) => {
   try {
